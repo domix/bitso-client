59cdba9840f580aca0e77247fa21694252e12ba3
Domingo Suarez Torres <domingo.suarez@gmail.com>
2017-04-22 00:52:02 -0500

    bitso api does not work

--- src/test/java/com/domingosuarez/bitso/client/Client.java
+++ src/test/java/com/domingosuarez/bitso/client/Client.java
@@ -1,19 +1,10 @@
 package com.domingosuarez.bitso.client;
 
-import com.bitso.Bitso;
 import org.junit.Test;
 
 public class Client {
   @Test
   public void testSomeLibraryMethod() {
-    Bitso bitso = new Bitso(null, null);
-    bitso.availableBooks().stream().forEach(bookInfo -> {
-      try {
-        System.out.println(bookInfo.book.name());
-      } catch (java.lang.IllegalArgumentException e) {
-
-      }
-    });
-
+    
   }
 }
