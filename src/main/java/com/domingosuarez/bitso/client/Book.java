941c12b5b42e4835b7a43d76bb2306c976ae9214
Domingo Suarez Torres <domingo.suarez@gmail.com>
2017-04-30 01:07:01 -0500

    added Book class

---
+++ src/main/java/com/domingosuarez/bitso/client/Book.java
@@ -0,0 +1,13 @@
+package com.domingosuarez.bitso.client;
+
+import java.math.BigDecimal;
+
+public class Book {
+  private String name;
+  private BigDecimal maxAmount;
+  private BigDecimal maxPrice;
+  private BigDecimal maxValue;
+  private BigDecimal minAmount;
+  private BigDecimal minPrice;
+  private BigDecimal minValue;
+}
